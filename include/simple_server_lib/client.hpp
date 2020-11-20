aae82178004aa6047202a19c9b8bc177ba5a76b1
progrm_jarvis <gh@progrm-jarvis.ru>
2020-11-20 19:40:45 +0300

    feat: add Client class implementation

---
+++ include/simple_server_lib/client.hpp
@@ -0,0 +1,115 @@
+// Copyright 2020 Petr Portnov <me@progrm-jarvis.ru>
+
+#ifndef INCLUDE_SIMPLE_SERVER_LIB_CLIENT_HPP_
+#define INCLUDE_SIMPLE_SERVER_LIB_CLIENT_HPP_
+
+#include <chrono>
+#include <simple_server_lib/definitions.hpp>
+#include <string>
+#include <variant>
+
+#include "user_manager.hpp"
+
+namespace simple_server_lib {
+
+    /**
+     * @brief Timeout after which the client should be considered dead
+     */
+    auto constexpr TIMEOUT = ::std::chrono::seconds(5);
+
+    /**
+     * @brief Client whose connection is handled
+     */
+    class Client final {
+    public:
+        /**
+         * @brief Type of socket object associated with this client
+         */
+        using socket_t = ip::tcp::socket;
+
+    private:
+        /**
+         * @brief Socket associated with the client
+         */
+        socket_t socket_;
+
+        /**
+         * @brief Manager of authenticated users
+         */
+        UserManager& user_manager_;
+
+        chrono::system_clock::time_point last_time_alive_;
+
+        struct NotAuthenticatedState final {};
+        struct AuthenticatedState final {
+            ::std::string name;
+        };
+
+        /**
+         * @brief Current state of this client
+         */
+        ::std::variant<NotAuthenticatedState, AuthenticatedState> state_;
+
+    public:
+        /**
+         * @brief Creates a new client
+         *
+         * @param executor executor used by this client's socket
+         * @param user_manager manager of the authenticated users
+         */
+        explicit Client(asio::io_service& executor, UserManager& user_manager);
+
+        ~Client();
+
+        /**
+         * @brief Handles this client
+         *
+         * @param error reference to filled error code
+         * @return {@code true} if this client is still alive and {@code false} otherwise
+         */
+        bool handle(error_code& error);
+
+        /**
+         * @brief Pings this client
+         *
+         * @param error reference to filled error code
+         */
+        void ping(error_code& error);
+
+        /**
+         * @brief Closes this client gracefully
+         */
+        void close();
+
+        /**
+         * @brief Gets a constant reference to the socket associated with the client
+         *
+         * @return constant reference to the socket associated with the client
+         */
+        [[nodiscard]] socket_t const& socket() const;
+
+        /**
+         * @brief Gets a reference to the socket associated with the client
+         *
+         * @return reference to the socket associated with the client
+         */
+        [[nodiscard]] socket_t& socket();
+
+    private:
+        /**
+         * @brief Sends the given payload to the client
+         *
+         * @param payload payload to send to the client
+         */
+        void send(::std::string const& payload, error_code& error);
+
+        /**
+         * @brief Disconnects the client sending the given payload
+         *
+         * @param payload payload to send to the client
+         */
+        void disconnect(::std::string const& payload, error_code& error);
+    };
+} // namespace simple_server_lib
+
+#endif // INCLUDE_SIMPLE_SERVER_LIB_CLIENT_HPP_
---
+++ sources/simple_server_lib/client.cpp
@@ -0,0 +1,143 @@
+// Copyright 2020 Petr Portnov <me@progrm-jarvis.ru>
+
+#include <boost/log/trivial.hpp>
+#include <simple_server_lib/client.hpp>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace simple_server_lib {
+
+    Client::Client(asio::io_service& executor, UserManager& user_manager)
+        : socket_{executor},
+          user_manager_(user_manager),
+          last_time_alive_{chrono::system_clock::now()},
+          state_{NotAuthenticatedState{}} {}
+
+    Client::~Client() { close(); }
+
+    void Client::close() { socket_.close(); }
+
+    template <typename D>
+    static ::std::vector<::std::string> split(::std::string const& string, D const& delimiter) {
+        ::std::vector<::std::string> result;
+
+        ::std::string::size_type start{0}, end;
+        while ((end = string.find(delimiter, start)) != ::std::string::npos) {
+            result.push_back(string.substr(start, end - start));
+            start = end + 1;
+        }
+        result.push_back(string.substr(start));
+
+        return result;
+    }
+
+    bool Client::handle(error_code& error) {
+        BOOST_LOG_TRIVIAL(info) << "<<HANDLE>>" << ::std::endl;
+        ::std::string line;
+        {
+            asio::streambuf buffer;
+            asio::read_until(socket_, buffer, DELIMITER, error);
+            if (error) {
+                if (error == asio::error::eof) {
+                    disconnect("disconnected", error);
+                    BOOST_LOG_TRIVIAL(info) << "User has disconnected" << ::std::endl;
+                    return false;
+                }
+                disconnect("buffer_error", error);
+                BOOST_LOG_TRIVIAL(info) << "Disconnecting user due to buffer error: " << error << ::std::endl;
+                return false;
+            }
+            ::std::istream input(&buffer);
+            ::std::getline(input, line);
+        }
+        auto const words = split(line, ' ');
+
+        if (words.empty()) {
+            disconnect("empty_packet", error);
+            BOOST_LOG_TRIVIAL(info) << "Disconnecting user due to empty payload" << ::std::endl;
+            return false;
+        }
+
+        for (auto const& word : words) { BOOST_LOG_TRIVIAL(info) << "\tword<" << word << ">" << ::std::endl; }
+        if (::std::holds_alternative<NotAuthenticatedState>(state_)) {
+            // User *must* authenticate
+
+            if (words[0] == "login") {
+                BOOST_LOG_TRIVIAL(info) << "Logging user" << ::std::endl;
+                if (words.size() > 1) {
+                    ::std::string username = words[1];
+                    {
+                        auto const size = words.size();
+                        for (::std::size_t i = 2; i < size; ++i) username += " " + words[i];
+                    }
+                    if (user_manager_.authenticate(username)) {
+                        state_ = AuthenticatedState{username};
+                        send("login ok", error);
+                        BOOST_LOG_TRIVIAL(info) << "Authenticated user by name \"" << username << "\"" << ::std::endl;
+                        return true;
+                    }
+
+                    disconnect("login already", error);
+                    BOOST_LOG_TRIVIAL(info) << "Disconnecting user due to username \"" << username
+                                            << "\" already being logged in by this username" << ::std::endl;
+                    return false;
+                }
+
+                disconnect("login missing_name", error);
+                BOOST_LOG_TRIVIAL(info) << "Disconnecting user due to missing username" << ::std::endl;
+                return false;
+            }
+
+            disconnect("not_authenticated", error);
+            BOOST_LOG_TRIVIAL(info) << "Disconnecting user due to him not being authenticated" << ::std::endl;
+            return false;
+        } else if (::std::holds_alternative<AuthenticatedState>(state_)) {
+            if (words[0] == "ping") {
+                ping(error);
+                BOOST_LOG_TRIVIAL(info) << "Pinged client" << ::std::endl;
+                return true;
+            }
+
+            if (words[0] == "clients") {
+                auto const usernames = user_manager_.get_authenticated();
+                assert(!usernames.empty() && "usernames cannot be empty");
+                auto iterator = usernames.cbegin();
+                ::std::string clients = *iterator;
+
+                auto const end = usernames.cend();
+                while (++iterator != end) clients += ", " + *iterator;
+
+                send(clients, error);
+                BOOST_LOG_TRIVIAL(info) << "Sent clients to the user" << ::std::endl;
+                return true;
+            }
+
+            disconnect("invalid_payload", error);
+            BOOST_LOG_TRIVIAL(info) << "Disconnecting user due to unknown payload" << ::std::endl;
+            return false;
+        }
+
+        throw ::std::runtime_error("Incorrect state");
+    }
+
+    void Client::ping(error_code& error) {
+        send("ping_ok", error);
+    }
+
+    void Client::send(::std::string const& payload, error_code& error) {
+        socket_.write_some(asio::buffer(payload + DELIMITER), error);
+    }
+
+    void Client::disconnect(::std::string const& payload, error_code& error) {
+        send(payload, error);
+        close();
+
+        if (::std::holds_alternative<AuthenticatedState>(state_))
+            user_manager_.unauthenticate(::std::get<AuthenticatedState>(state_).name);
+    }
+
+    Client::socket_t const& Client::socket() const { return socket_; }
+
+    Client::socket_t& Client::socket() { return socket_; }
+} // namespace simple_server_lib
